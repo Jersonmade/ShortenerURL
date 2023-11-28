30c07ec57a3bcf241f15d59ca7ecb06b2467fdf8
Jersonmade <dima.sergeev.0404@inbox.ru>
2023-11-28 16:48:09 +0400

    init

---
+++ src/main/java/com/jersonmade/shortener/ShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.jersonmade.shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenerApplication.class, args);
+	}
+
+}
